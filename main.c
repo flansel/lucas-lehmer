f1d74d7e5450bc6777e784e32dd72295a8ed228a
flansel <fjansell@gmail.com>
2019-09-09 09:31:09 -0400

    fixed indents

--- main.c
+++ main.c
@@ -11,16 +11,16 @@ int main(int argc, char* argv[]){
 }
 
 int checkPrimeFaster(int num){
-        if(num == 0 || num == 1){
-                return -1;
-  }
+	if(num == 0 || num == 1){
+    	return -1;
+	}
   double root = sqrt((double)num);
   root = (int) root;
   int i;
   for(i = 2; i<=root; i++){
-        if(num % i == 0){
-                return i;
-        }
+  	if(num % i == 0){
+    	return i;
+  	}
   }
   return 0;
 }
@@ -54,55 +54,55 @@ bool lucasLehmer(int exp){
 }
 
 bool findLucasLehmerNumber(int exp, mpz_t mod){
-        printf("Starting Lucas-Lehmer Sequence\n");
+	printf("Starting Lucas-Lehmer Sequence\n");
+    mpz_t n;
+    int flag;
+    mpz_init(n);
+    mpz_set_ui(n,0);
+    flag = mpz_set_str(n,"4",10);
+    if(flag != 0){
+    	printf("Lucas-Lehmer Sequence creation FAILED\n");
+        exit(0);
+    }
+    int i;
+    for(i = 0;i<exp-2;i++){         
+    	mpz_pow_ui(n,n,2);
+        mpz_sub_ui(n,n,2);
+        mpz_mod(n,n,mod);
+    }       
+    printf("Found Lucas Number-->\n");
+    mpz_out_str(stdout,10,n);
+    printf("\n\n");
+    if(mpz_cmp_ui(n,0) == 0){
+    	return true;
+    }else{
+       	return false;
+    }
+    return 0;
+}
+
+void lucasSetup(){
+	char exp [32];
+    printf("Enter the power of the Mersenne Prime\n");
+    scanf("%32s",exp);
+    int expNum = atoi(exp);
+    if(checkPrimeFaster(expNum) == 0){
+    	lucasLehmer(expNum);
+    }else{
+        printf("Aborting --> exp not prime\n");
         mpz_t n;
         int flag;
         mpz_init(n);
         mpz_set_ui(n,0);
-        flag = mpz_set_str(n,"4",10);
+        flag = mpz_set_str(n,"2", 10);
         if(flag != 0){
-                printf("Lucas-Lehmer Sequence creation FAILED\n");
-                exit(0);
-        }
-        int i;
-        for(i = 0;i<exp-2;i++){         
-                mpz_pow_ui(n,n,2);
-                mpz_sub_ui(n,n,2);
-                mpz_mod(n,n,mod);
+        	printf("Failed\n");
+            exit(0);
         }       
-        printf("Found Lucas Number-->\n");
+       	mpz_pow_ui(n,n,expNum);
+        mpz_sub_ui(n,n,1);
         mpz_out_str(stdout,10,n);
-        printf("\n\n");
-        if(mpz_cmp_ui(n,0) == 0){
-            return true;
-        } else {
-        	return false;
-        }
-        return 0;
-}
-
-void lucasSetup(){
-        char exp [32];
-        printf("Enter the power of the Mersenne Prime\n");
-        scanf("%32s",exp);
-        int expNum = atoi(exp);
-        if(checkPrimeFaster(expNum) == 0){
-                lucasLehmer(expNum);
-        }else{
-                printf("Aborting --> exp not prime\n");
-                mpz_t n;
-                int flag;
-                mpz_init(n);
-                mpz_set_ui(n,0);
-                flag = mpz_set_str(n,"2", 10);
-                if(flag != 0){
-                        printf("Failed\n");
-                        exit(0);
-                }       
-                mpz_pow_ui(n,n,expNum);
-                mpz_sub_ui(n,n,1);
-                mpz_out_str(stdout,10,n);
-                printf (" is definitly not prime \n");
-        }
+        printf (" is definitly not prime \n");
+   	}
 }
 
